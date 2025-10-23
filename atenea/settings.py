ac01fa1e0cffee6b1343a50da6d1fb066a6dabe8
David <dev.idbarreiro@gmail.com>
2025-10-23 11:55:05 +0000

    chore: update database configuration to use PostgreSQL
    - Changed database engine from SQLite to PostgreSQL.
    - Updated database connection settings to use environment variables for database name, user, password, host, and port.

--- atenea/settings.py
+++ atenea/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'atenea.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB', default='atenea'),
+        'USER': config('POSTGRES_USER', default='atenea'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('DB_HOST', default='db'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
