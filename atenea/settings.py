3e9e9e7bbf076cab943179b8e303ed5e76416559
David Barreiro <dev.idbarreiro@gmail.com>
2025-11-27 12:45:28 +0100

    feat: añadir nuevos dominios ailumtech a ALLOWED_HOSTS y CSRF_TRUSTED_ORIGINS

--- atenea/settings.py
+++ atenea/settings.py
@@ -33,7 +33,25 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+# ALLOWED_HOSTS: dominios permitidos para la aplicación
+# Se puede configurar via variable de entorno o usar los conocidos
+allowed_hosts_env = config('ALLOWED_HOSTS', default='')
+if allowed_hosts_env:
+    ALLOWED_HOSTS = allowed_hosts_env.split(',')
+else:
+    # Dominios por defecto (desarrollo)
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+    # Añadir dominios de producción conocidos
+    production_hosts = [
+        'dev.atenea.nxhumans.com',
+        'demo.atenea.nxhumans.com',
+        'atenea.nxhumans.com',
+        'dev.atenea.ailumtech.com',
+        'demo.atenea.ailumtech.com',
+        'test.atenea.ailumtech.com',
+        'npm.atenea.ailumtech.com',
+    ]
+    ALLOWED_HOSTS.extend(production_hosts)
 
 # CSRF Trusted Origins
 # Añadir los dominios desde los que se pueden hacer peticiones POST
@@ -50,13 +68,21 @@ if csrf_origins_env:
 else:
     # Si no hay variable de entorno, usar los dominios conocidos
     CSRF_TRUSTED_ORIGINS = default_csrf_origins.copy()
-    # Añadir dominios de producción conocidos
+    # Añadir dominios de producción conocidos (nxhumans)
     production_domains = [
         'https://dev.atenea.nxhumans.com',
         'https://demo.atenea.nxhumans.com',
         'https://atenea.nxhumans.com',
     ]
     CSRF_TRUSTED_ORIGINS.extend(production_domains)
+    # Añadir dominios de producción conocidos (ailumtech)
+    production_domains_ailumtech = [
+        'https://dev.atenea.ailumtech.com',
+        'https://demo.atenea.ailumtech.com',
+        'https://test.atenea.ailumtech.com',
+        'https://npm.atenea.ailumtech.com',
+    ]
+    CSRF_TRUSTED_ORIGINS.extend(production_domains_ailumtech)
 
 
 # Application definition
