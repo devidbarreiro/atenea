25ad5f1f577cf0548b7029a9892a28b0e7296eb5
David Barreiro <dev.idbarreiro@gmail.com>
2025-12-02 15:09:58 +0100

    chore: limpiar import innecesario de AllowedHostsOriginValidator

--- atenea/asgi.py
+++ atenea/asgi.py
@@ -11,7 +11,6 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atenea.settings')
 
